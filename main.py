5c8d32f72444c9c1b1ff26c398c6d217b42a3c14
Akshay Kumar <akshaykumarkolhar@gmail.com>
2026-04-23 15:07:13 +0530

    updating code

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from unittest import result
+
 import numpy as np
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer 
 from sklearn.svm import LinearSVC
+
 
 data=pd.read_csv("fake_or_real_news.csv")
 
@@ -12,6 +15,7 @@ data["fake"]=data["label"].apply(lambda x:1 if x=="REAL" else 0)
 data=data.drop("label", axis=1)
 
 x,y=data["text"], data["fake"]
+
 x_train, x_test, y_train, y_test=train_test_split(x,y, test_size=0.2)
 
 
@@ -22,14 +26,14 @@ x_test_vectorized=vectorizer.transform(x_test)
 clf=LinearSVC()
 clf.fit(x_train_vectorized, y_train)
 
-with open("my_text.txt","w") as f:
-    f.write(x_test.iloc[0])
-with open ("my_text.txt","r") as f:
-    text=f.read()
 
-vectorized_text=vectorizer.transform([text])
-result=clf.predict(vectorized_text)
+text=input("Enter the news you want to check: ")
+    
+
+vectorizer_input=vectorizer.transform([text])
+
+result=clf.predict(vectorizer_input)
+
 if result[0]==1:
-    print("The news is REAL")
-else:
-    print("The news is FAKE")
+    print("The news is real")
+else:    print("The news is fake")
